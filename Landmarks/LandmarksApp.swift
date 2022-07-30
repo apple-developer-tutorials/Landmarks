4cae1e4c6d9c92f5fb512734911d4b37099ccfbc
Chris Hand <cj.hand@gmail.com>
2022-07-29 22:50:51 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chris Hand on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
