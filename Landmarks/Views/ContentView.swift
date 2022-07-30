279efd49ef5a73686dc86e01f18f08a2323aca74
Chris Hand <cj.hand@gmail.com>
2022-07-29 23:22:57 -0700

    Show park and state. Create Model View folders

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Chris Hand on 7/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            
+            HStack {
+                Text("Joshua Tree National Park")
+                Spacer()
+                Text("California")
+            }
+            .font(.subheadline)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
