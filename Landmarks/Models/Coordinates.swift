847b356397739e11986d2ee787e8af7d1aff4ba6
Chris Hand <cj.hand@gmail.com>
2022-07-30 00:14:03 -0700

    Read the landmark.json data file

---
+++ Landmarks/Models/Coordinates.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinates.swift
+//  Landmarks
+//
+//  Created by Chris Hand on 7/30/22.
+//
+
+import Foundation
+
+struct Coordinates : Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
---
+++ Landmarks/Models/Landmark.swift
@@ -0,0 +1,31 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Chris Hand on 7/30/22.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+}
