25049511c5e61c704d52df9b5600fbc5bb293033
Chris Hand <cj.hand@gmail.com>
2022-08-02 22:09:45 -0700

    Read in the categories

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -7,9 +7,16 @@
 
 import SwiftUI
 
-struct CategoriesHome: View {
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
     var body: some View {
         NavigationView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
             Text("Hello, Chris")
                 .navigationTitle("Featured")
         }
@@ -18,6 +25,7 @@ struct CategoriesHome: View {
 
 struct CategoriesHome_Previews: PreviewProvider {
     static var previews: some View {
-        CategoriesHome()
+        CategoryHome()
+            .environmentObject(ModelData())
     }
 }
