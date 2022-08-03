7d5a1c0c562091842b6a97d70e305f6c76237a43
Chris Hand <cj.hand@gmail.com>
2022-08-02 22:02:37 -0700

    Create a categories home

---
+++ Landmarks/Views/Categories/CategoriesHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoriesHome.swift
+//  Landmarks
+//
+//  Created by Chris Hand on 8/2/22.
+//
+
+import SwiftUI
+
+struct CategoriesHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, Chris")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoriesHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoriesHome()
+    }
+}
