7c77cece81a14f76fc6fe0937a4aaf6ba7bea51f
Chris Hand <cj.hand@gmail.com>
2022-08-10 22:16:15 -0700

    Add a macOS target to the project

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Chris Hand on 8/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
