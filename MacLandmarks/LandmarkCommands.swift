0c596c7de87f77e71c4da5b694e157622783ceb0
Chris Hand <cj.hand@gmail.com>
2022-08-11 22:41:14 -0700

    Add a build in menu command

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  MacLandmarks
+//
+//  Created by Chris Hand on 8/11/22.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
